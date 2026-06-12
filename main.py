e134ea057bd3ef3fcf7766375f980acac7957198
natan <natancorreiatr@gmail.com>
2026-06-11 21:16:10 -0300

    fix: cors

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from starlette import status
 
@@ -15,6 +16,14 @@ load_dotenv()
 
 app = FastAPI(title="Verdade ou Fake")
 app.include_router(fact_check_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(RequestValidationError)
